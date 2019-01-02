d68cc6b0f85abb629049fa3529cbd52487387d6e
Gagandeep Singh <Gagan@Gagandeeps-MacBook-Pro.local>
2019-01-02 19:19:03 +0530

    constructor function

--- index.js
+++ index.js
@@ -8,5 +8,22 @@ function createCircle(radius) {
     };
 }
 
-const circle = createCircle(9);
-circle.draw();
+//const circle = createCircle(9);
+
+//constructor function
+// does three things
+// 1. create an empty object {}
+// 2. pass this empty object as this inside the function
+// 3. return this object from the function p
+function Circle(radius) {
+    console.log(this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+
+    // The below statement is implicit
+    //return this
+} 
+
+const circle = new Circle(9);
