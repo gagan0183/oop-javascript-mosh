7abe9f590da40970babe12578bf9f9e6f58010be
Gagandeep Singh <Gagan@Gagandeeps-MacBook-Pro.local>
2019-01-04 01:44:00 +0530

    prototype

--- prototype.js
+++ prototype.js
@@ -16,3 +16,20 @@ console.log(person);
 
 //cannot enumeration the property
 console.log(Object.keys(person));
+
+function Circle(radius) {
+    //instance members
+    this.radius = radius;
+}
+
+//prototype members
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+const c1 = new Circle(9);
+const c2 = new Circle(9);
+
+Circle.prototype.toString = function() {
+    return 'circle with radius ' + this.radius;
+}
