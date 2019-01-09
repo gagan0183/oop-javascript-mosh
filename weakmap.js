949b07a50d2dc88fd89bdf057f99fa9eb19b48ec
Gagandeep Singh <Gagan@Gagandeeps-MacBook-Pro.local>
2019-01-09 12:30:40 +0530

    weamap

--- weakmap.js
+++ weakmap.js
@@ -16,7 +16,18 @@ class Circle {
         console.log(privateProps.get(this).radius);
         console.log(privateProps.get(this).move());
     }
+
+    //getter and setter properties
+
+    get radius() {
+        return privateProps.get(this).radius;
+    }
+
+    set radius(val) {
+        privateProps.set(this,)
+    }
 }
 
 const c = new Circle(9);
 c.draw();
+console.log(c.radius);
